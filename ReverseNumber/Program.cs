29a43d11c104c47c1e5d00dee79a5b96afdd2460
unisrn0110 <unisrn0110@gmail.com>
2017-10-09 01:27:19 +0700

    no loop input

--- ReverseNumber/Program.cs
+++ ReverseNumber/Program.cs
@@ -10,9 +10,54 @@ namespace ReverseNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            string strNumber = "";
+
+            do
+            {
+                Console.Write("Enter a number (1000 - 9999): ");
+                strNumber = Console.ReadLine();
+
+                if (strNumber == "0")
+                {
+                    break;
+                }
+            } while (!validation(strNumber));
+            
+
+            int strConvertToNumber = Convert.ToInt32(ReverseNumber(strNumber));
+            if (Convert.ToInt32(strNumber) > strConvertToNumber)
+            {
+                int result = Convert.ToInt32(strNumber) - strConvertToNumber;
+                Console.WriteLine(strNumber + " - " + strConvertToNumber + " = " + result);
+            }
+            else
+            {
+                int result = strConvertToNumber - Convert.ToInt32(strNumber);
+                Console.WriteLine(strConvertToNumber + " - " + strNumber + " = " + result);
+            }
 
             Console.ReadKey();
+        }
+
+        public static string ReverseNumber(string strNumber)
+        {
+            string concat = "";
+            int len = strNumber.Length;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                concat += strNumber[i];
+            }
+            return concat;
+        }
+
+        public static bool validation(string strNumber)
+        {
+            if (strNumber == "")
+            {
+                Console.WriteLine("Please enter number between 1000 - 9999");
+                return false;
+            }
+            return true;
         }
     }
 }
