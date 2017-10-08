17ca6b6f4cab31a83cf00373ccd7a3b16fd552b3
unisrn0110 <unisrn0110@gmail.com>
2017-10-09 00:27:58 +0700

    commit console

--- ReverseNumber/Program.cs
+++ ReverseNumber/Program.cs
@@ -10,6 +10,9 @@ namespace ReverseNumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+
+            Console.ReadKey();
         }
     }
 }
